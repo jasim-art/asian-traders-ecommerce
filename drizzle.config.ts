9b1b060bf8f489add0c40b1e49cf661cf6aef52a
jasim-art <msjasim959@gmail.com>
2026-09-05 11:47:56 +0530

    Fix database configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "drizzle-kit";
-import "dotenv/config";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
 
 export default {
   schema: "./src/db/schema.ts",
